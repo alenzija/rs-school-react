d62c8cd39381aa7184a908f3137fc8e53e7da730
Alena <alenzija@gmail.com>
2023-10-30 12:49:56 +0100

    refactor: delete comments

--- src/components/error-boundary/index.tsx
+++ src/components/error-boundary/index.tsx
@@ -8,23 +8,15 @@ class ErrorBoundary extends Component<Readonly<{ children: ReactNode }>> {
   };
 
   static getDerivedStateFromError() {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
   componentDidCatch(error: Error) {
-    // Example "componentStack":
-    //   in ComponentThatThrows (created by App)
-    //   in ErrorBoundary (created by App)
-    //   in div (created by App)
-    //   in App
     console.log('ErrorBoundary did catch', error.message);
-    //logErrorToMyService(error, info.componentStack);
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return <ErrorMessage />;
     }
 
